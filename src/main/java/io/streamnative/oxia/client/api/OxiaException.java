518211e9856ce4f1e1b00c9e0008640c21560204
teabot <elliot.west@streamnative.io>
2023-01-12 13:46:08 +0000

    Typo

--- src/main/java/io/streamnative/oxia/client/api/OxiaException.java
+++ src/main/java/io/streamnative/oxia/client/api/OxiaException.java
@@ -1,6 +1,6 @@
 package io.streamnative.oxia.client.api;
 
-/** A super-class of exceptions describing errors that occured on an Oxia server. */
+/** A super-class of exceptions describing errors that occurred on an Oxia server. */
 public abstract class OxiaException extends Exception {
     OxiaException(String message) {
         super(message);
