2cca7c741f38c80bd36f1cd0ba808027b1b650ce
teabot <elliot.west@streamnative.io>
2023-01-12 14:55:28 +0000

    Missed commit

--- src/main/java/io/streamnative/oxia/client/api/SyncOxiaClient.java
+++ src/main/java/io/streamnative/oxia/client/api/SyncOxiaClient.java
@@ -15,7 +15,7 @@ public interface SyncOxiaClient extends AutoCloseable {
      * @param key The key with which the payload should be associated.
      * @param payload The payload to associate with the key.
      * @param expectedVersion The version of the record that this put operation is scoped to.
-     * @return The result of the put at the specified key, or {@code null} if the key did not exist.
+     * @return The result of the put at the specified key.
      * @throws UnexpectedVersionException The version at the server did not that match supplied in the
      *     call.
      */
@@ -27,7 +27,7 @@ public interface SyncOxiaClient extends AutoCloseable {
      *
      * @param key The key with which the payload should be associated.
      * @param payload The payload to associate with the key.
-     * @return The result of the put at the specified key, or {@code null} if the key did not exist.
+     * @return The result of the put at the specified key.
      */
     PutResult put(@NonNull String key, byte @NonNull [] payload);
 
@@ -39,10 +39,11 @@ public interface SyncOxiaClient extends AutoCloseable {
      *
      * @param key Deletes the record with the specified key.
      * @param expectedVersion The version of the record that this delete operation is scoped to.
+     * @return True if the key was actually present on the server, false otherwise.
      * @throws UnexpectedVersionException The version at the server did not that match supplied in the
      *     call.
      */
-    void delete(@NonNull String key, long expectedVersion) throws UnexpectedVersionException;
+    boolean delete(@NonNull String key, long expectedVersion) throws UnexpectedVersionException;
 
     /**
      * Deletes the record associated with the key if the record exists.
@@ -54,7 +55,8 @@ public interface SyncOxiaClient extends AutoCloseable {
 
     /**
      * Deletes any records with keys within the specified range. TODO describe the ordering of keys
-     * within the range.
+     * within the range, but for now see: <a
+     * href="https://github.com/streamnative/oxia/blob/main/server/kv/kv_pebble.go#L468-L499">GitHub</a>.
      *
      * @param minKeyInclusive The key that declares start of the range, and is <b>included</b> from
      *     the range.
@@ -74,7 +76,8 @@ public interface SyncOxiaClient extends AutoCloseable {
 
     /**
      * Lists any existing keys within the specified range. TODO describe the ordering of keys within
-     * the range.
+     * the range, but for now see: <a
+     * href="https://github.com/streamnative/oxia/blob/main/server/kv/kv_pebble.go#L468-L499">GitHub</a>.
      *
      * @param minKeyInclusive The key that declares start of the range, and is <b>included</b> from
      *     the range.
