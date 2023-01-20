ea76fd97c8435af8817a5739f481307e9ef2439b
teabot <elliot.west@streamnative.io>
2023-01-20 10:13:31 +0000

    Inline conversions

--- client/src/main/java/io/streamnative/oxia/client/ProtoUtil.java
+++ client/src/main/java/io/streamnative/oxia/client/ProtoUtil.java
@@ -1,16 +1,24 @@
 package io.streamnative.oxia.client;
 
+
+import java.nio.ByteBuffer;
 import java.util.function.Consumer;
+import lombok.NonNull;
 
 public class ProtoUtil {
-    public static long VersionNotExists = -1;
+    public static long versionNotExists = -1;
 
     public static int longToUint32(long value) {
-        return -1;
+        return ByteBuffer.allocate(8).putLong(value).position(4).getInt();
     }
 
-    public static void setOptionalExpectedVersion(long expectedVersion, Consumer<Long> setterFn) {
-        if (expectedVersion > VersionNotExists) {
+    public static long uint32ToLong(int unit32AsInt) {
+        return ByteBuffer.allocate(8).putInt(0).putInt(unit32AsInt).flip().getLong();
+    }
+
+    public static void setOptionalExpectedVersion(
+            long expectedVersion, @NonNull Consumer<Long> setterFn) {
+        if (expectedVersion > versionNotExists) {
             setterFn.accept(expectedVersion);
         }
     }
