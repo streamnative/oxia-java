5473557fb9efb3ff47a77109fb2b1ad20cb74206
Andras Beni <andras.beni@streamnative.io>
2023-01-24 11:54:05 +0100

    Add OxiaContainer

---
+++ testcontainers/src/main/java/io/streamnative/oxia/testcontainers/OxiaContainer.java
@@ -0,0 +1,64 @@
+package io.streamnative.oxia.testcontainers;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
+
+import java.time.Duration;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
+import org.testcontainers.utility.DockerImageName;
+
+public class OxiaContainer extends GenericContainer<OxiaContainer> {
+
+    public static final int OXIA_PORT = 6648;
+    public static final int METRICS_PORT = 8080;
+
+    public static final String DEFAULT_IMAGE_NAME = "oxia:latest";
+
+    private final String containerName;
+
+    public OxiaContainer() {
+        this(DockerImageName.parse(DEFAULT_IMAGE_NAME), "oxia-container");
+    }
+
+    public OxiaContainer(DockerImageName imageName, String containerName) {
+        super(imageName);
+        this.containerName = containerName;
+    }
+
+    @Override
+    public String getContainerName() {
+        return containerName;
+    }
+
+    @Override
+    protected void configure() {
+        super.configure();
+        withExposedPorts(OXIA_PORT, METRICS_PORT);
+    }
+
+    @Override
+    public void start() {
+        this.waitStrategy =
+                new HttpWaitStrategy()
+                        .forPort(METRICS_PORT)
+                        .forStatusCode(200)
+                        .forPath("/metrics")
+                        .withStartupTimeout(Duration.of(30, SECONDS));
+        this.withCreateContainerCmdModifier(
+                createContainerCmd -> {
+                    createContainerCmd.withHostName(containerName);
+                    createContainerCmd.withName(getContainerName());
+                    createContainerCmd.withEntrypoint("/oxia/bin/oxia", "standalone");
+                });
+
+        super.start();
+    }
+
+    public String getServiceAddress() {
+        return getHost() + ":" + getMappedPort(OXIA_PORT);
+    }
+
+    public String getMetricsUrl() {
+        return "http://" + getHost() + ":" + getMappedPort(METRICS_PORT) + "/metrics";
+    }
+}
---
+++ testcontainers/src/test/java/io/streamnative/oxia/testcontainers/OxiaContainerTest.java
@@ -0,0 +1,40 @@
+package io.streamnative.oxia.testcontainers;
+
+
+import io.streamnative.oxia.client.OxiaClientBuilder;
+import java.nio.charset.StandardCharsets;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class OxiaContainerTest {
+
+    private static OxiaContainer container;
+
+    @BeforeClass
+    public static void setupOxia() {
+        container = new OxiaContainer();
+        container.start();
+    }
+
+    @AfterClass
+    public static void tearDownOxia() {
+        if (container != null) {
+            container.stop();
+        }
+    }
+
+    @Test
+    public void test() throws Exception {
+
+        try (var client = new OxiaClientBuilder(container.getServiceAddress()).syncClient()) {
+            client.put("hello", "world".getBytes(StandardCharsets.UTF_8), -1);
+            var result = client.get("hello");
+            Assert.assertEquals("world", new String(result.payload()));
+
+        } catch (Exception e) {
+            // TODO remove this catch block when client supports put and get
+        }
+    }
+}
