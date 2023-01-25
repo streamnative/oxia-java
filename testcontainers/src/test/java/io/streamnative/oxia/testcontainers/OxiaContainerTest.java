462a028ce144235a1487858527a5f3401d8fc6c1
Andras Beni <andras.beni@streamnative.io>
2023-01-25 16:38:14 +0100

    Run the cli from another container

--- testcontainers/src/test/java/io/streamnative/oxia/testcontainers/OxiaContainerTest.java
+++ testcontainers/src/test/java/io/streamnative/oxia/testcontainers/OxiaContainerTest.java
@@ -11,15 +11,20 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.testcontainers.containers.Network;
 
 @Ignore("First we need to make sure we can access the docker image")
 public class OxiaContainerTest {
 
+    private static final String HOST = "oxia";
     private static OxiaContainer container;
 
     @BeforeClass
     public static void setupOxia() {
-        container = new OxiaContainer(OxiaContainer.DEFAULT_IMAGE_NAME);
+        container =
+                new OxiaContainer(OxiaContainer.DEFAULT_IMAGE_NAME)
+                        .withNetwork(Network.newNetwork())
+                        .withNetworkAliases(HOST);
         container.start();
     }
 
@@ -32,13 +37,24 @@ public class OxiaContainerTest {
 
     @Test
     public void testPutGetWithCLI() throws Exception {
-        var result = container.execInContainer("oxia", "client", "put", "-k", "hello", "-v", "world");
-        assertEquals(0, result.getExitCode());
-        result = container.execInContainer("oxia", "client", "get", "-k", "hello");
-        assertEquals(0, result.getExitCode());
-        var stdOut = result.getStdout();
-        assertTrue(
-                "Get should retrieve the value put before. Output: " + stdOut, stdOut.contains("world"));
+        var clientContainer =
+                new OxiaContainer(OxiaContainer.DEFAULT_IMAGE_NAME).withNetwork(container.getNetwork());
+        var address = HOST + ":" + OxiaContainer.OXIA_PORT;
+        try {
+            clientContainer.start();
+            var result =
+                    clientContainer.execInContainer(
+                            "oxia", "-a", address, "client", "put", "-k", "hello", "-v", "world");
+            assertEquals("Put should create the record. Output: " + result, 0, result.getExitCode());
+            result =
+                    clientContainer.execInContainer("oxia", "-a", address, "client", "get", "-k", "hello");
+            assertEquals(0, result.getExitCode());
+            var stdOut = result.getStdout();
+            assertTrue(
+                    "Get should retrieve the value put before. Output: " + stdOut, stdOut.contains("world"));
+        } finally {
+            clientContainer.stop();
+        }
     }
 
     @Test
