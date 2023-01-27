c83bba6cabedb634bd55e3ac12ee3b65d8920b76
teabot <elliot.west@streamnative.io>
2023-01-27 15:48:57 +0000

    Extending IT case

--- client/src/test/java/io/streamnative/oxia/client/OxiaClientIT.java
+++ client/src/test/java/io/streamnative/oxia/client/OxiaClientIT.java
@@ -19,9 +19,17 @@ import static io.streamnative.oxia.testcontainers.OxiaContainer.DEFAULT_IMAGE_NA
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.concurrent.CompletableFuture.allOf;
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 import io.streamnative.oxia.client.api.AsyncOxiaClient;
+import io.streamnative.oxia.client.api.Notification;
+import io.streamnative.oxia.client.api.Notification.KeyCreated;
+import io.streamnative.oxia.client.api.Notification.KeyDeleted;
+import io.streamnative.oxia.client.api.Notification.KeyModified;
+import io.streamnative.oxia.client.api.UnexpectedVersionIdException;
 import io.streamnative.oxia.testcontainers.OxiaContainer;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -34,9 +42,15 @@ public class OxiaClientIT {
 
     private static AsyncOxiaClient client;
 
+    private static List<Notification> notifications = new ArrayList<>();
+
     @BeforeAll
     static void beforeAll() {
-        client = new OxiaClientBuilder(oxia.getServiceAddress()).standalone().asyncClient();
+        client =
+                new OxiaClientBuilder(oxia.getServiceAddress())
+                        .standalone()
+                        .notificationCallback(n -> notifications.add(n))
+                        .asyncClient();
     }
 
     @AfterAll
@@ -59,11 +73,29 @@ public class OxiaClientIT {
         assertThat(getResult.getValue()).isEqualTo("a".getBytes(UTF_8));
         var aVersion = getResult.getVersion().versionId();
 
+        // verify notification for 'a'
+        long finalAVersion = aVersion;
+        await().untilAsserted(() -> assertThat(notifications).contains(new KeyCreated("a", finalAVersion)));
+
         // update 'a' with expected version
         client.put("a", "a2".getBytes(UTF_8), aVersion).join();
         getResult = client.get("a").join();
         assertThat(getResult.getValue()).isEqualTo("a2".getBytes(UTF_8));
         aVersion = getResult.getVersion().versionId();
+
+        // verify notification for 'a' update
+        long finalA2Version = aVersion;
+        await().untilAsserted(() -> assertThat(notifications).contains(new KeyModified("a", finalA2Version)));
+
+        // put with unexpected version
+        var bVersion = client.get("b").join().getVersion().versionId();
+        assertThatThrownBy(() -> client.put("b", "b2".getBytes(UTF_8), bVersion + 1L))
+                .isInstanceOf(UnexpectedVersionIdException.class);
+
+        // delete with unexpected version
+        var cVersion = client.get("c").join().getVersion().versionId();
+        assertThatThrownBy(() -> client.delete("c", cVersion + 1L))
+                .isInstanceOf(UnexpectedVersionIdException.class);
 
         // list all keys
         var listResult = client.list("a", "e").join();
@@ -73,6 +105,10 @@ public class OxiaClientIT {
         client.delete("a", aVersion).join();
         getResult = client.get("a").join();
         assertThat(getResult).isNull();
+
+        // verify notification for 'a' update
+        long finalADVersion = aVersion;
+        await().untilAsserted(() -> assertThat(notifications).contains(new KeyDeleted("a", finalADVersion)));
 
         // delete 'b'
         client.delete("b").join();
@@ -85,5 +121,8 @@ public class OxiaClientIT {
         // list all keys
         listResult = client.list("a", "e").join();
         assertThat(listResult).containsExactly("d");
+
+        // get non-existent key
+        assertThat(client.get("z").join()).isNull();
     }
 }
