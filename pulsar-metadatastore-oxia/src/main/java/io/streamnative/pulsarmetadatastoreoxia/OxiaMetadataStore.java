4b8e2abe843d6a890d019167fdb50dfd57ee1773
teabot <elliot.west@streamnative.io>
2023-03-06 11:54:56 +0000

    Disable cache to investigate test failure

--- pulsar-metadatastore-oxia/src/main/java/io/streamnative/pulsarmetadatastoreoxia/OxiaMetadataStore.java
+++ pulsar-metadatastore-oxia/src/main/java/io/streamnative/pulsarmetadatastoreoxia/OxiaMetadataStore.java
@@ -69,6 +69,7 @@ public class OxiaMetadataStore extends AbstractMetadataStore {
                         .batchLinger(Duration.ofMillis(linger))
                         .maxRequestsPerBatch(metadataStoreConfig.getBatchingMaxOperations())
                         .operationQueueCapacity(4000)
+                        .recordCacheCapacity(0)
                         .asyncClient()
                         .get();
         client.notifications(this::notificationCallback);
