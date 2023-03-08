489a07d8edb8ed7ce11771ff5ed9db3c0b56212e
teabot <elliot.west@streamnative.io>
2023-03-08 11:28:04 +0000

    Fixed typo and added test case

--- client/src/main/java/io/streamnative/oxia/client/session/Session.java
+++ client/src/main/java/io/streamnative/oxia/client/session/Session.java
@@ -19,6 +19,7 @@ import static io.streamnative.oxia.client.ProtoUtil.longToUint32;
 import static lombok.AccessLevel.PACKAGE;
 import static lombok.AccessLevel.PUBLIC;
 
+import com.google.common.annotations.VisibleForTesting;
 import io.streamnative.oxia.client.ClientConfig;
 import io.streamnative.oxia.proto.CloseSessionRequest;
 import io.streamnative.oxia.proto.CreateSessionRequest;
@@ -43,6 +44,9 @@ public class Session implements AutoCloseable {
     private final @NonNull Function<Long, ReactorOxiaClientGrpc.ReactorOxiaClientStub> stubByShardId;
     private final @NonNull Duration sessionTimeout;
     private final @NonNull Duration heartbeatInterval;
+
+    @Getter(PACKAGE)
+    @VisibleForTesting
     private final long shardId;
 
     @Getter(PUBLIC)
@@ -65,7 +69,7 @@ public class Session implements AutoCloseable {
                 shardId,
                 sessionId,
                 SessionHeartbeat.newBuilder()
-                        .setShardId(longToUint32(sessionId))
+                        .setShardId(longToUint32(shardId))
                         .setSessionId(sessionId)
                         .build());
     }
