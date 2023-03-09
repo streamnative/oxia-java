4426b82e8b7c02bab632150c8817cd606a11aa32
teabot <elliot.west@streamnative.io>
2023-03-09 13:05:16 +0000

    Do not register unchanged shards as an overlap

--- client/src/test/java/io/streamnative/oxia/client/shard/ShardTest.java
+++ client/src/test/java/io/streamnative/oxia/client/shard/ShardTest.java
@@ -19,7 +19,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.streamnative.oxia.proto.Int32HashRange;
 import io.streamnative.oxia.proto.ShardAssignment;
+import java.util.Set;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ShardTest {
 
@@ -46,5 +51,33 @@ class ShardTest {
                                                 assertThat(h.maxInclusive()).isEqualTo(3L);
                                             });
                         });
+    }
+
+    private static Stream<Arguments> findOverlappingArgs() {
+        return Stream.of(
+                // Equal
+                Arguments.of(shard(0, 0, 0, 0), Set.of(shard(0, 0, 0, 0)), Set.of()),
+                // Leader change
+                Arguments.of(shard(0, 0, 0, 0), Set.of(shard(0, 1, 0, 0)), Set.of(shard(0, 1, 0, 0))),
+                // ID change
+                Arguments.of(shard(0, 0, 0, 0), Set.of(shard(1, 0, 0, 0)), Set.of(shard(1, 0, 0, 0))),
+                // No overlap - although ID remains the same - probably an error condition?
+                Arguments.of(shard(0, 0, 0, 0), Set.of(shard(0, 0, 1, 1)), Set.of()),
+                // Partial overlaps
+                Arguments.of(
+                        shard(0, 0, 1, 2),
+                        Set.of(shard(1, 0, 0, 1), shard(2, 0, 2, 3)),
+                        Set.of(shard(1, 0, 0, 1), shard(2, 0, 2, 3))));
+    }
+
+    @ParameterizedTest
+    @MethodSource("findOverlappingArgs")
+    void findOverlapping(Shard target, Set<Shard> updates, Set<Shard> overlaps) {
+        assertThat(target.findOverlapping(updates))
+                .containsExactlyInAnyOrder(overlaps.toArray(overlaps.toArray(new Shard[0])));
+    }
+
+    static Shard shard(long id, int leader, long min, long max) {
+        return new Shard(id, Integer.toString(leader), new HashRange(min, max));
     }
 }
