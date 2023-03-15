3c0796520b9c765fdcc82a9950c02485194cbe8c
Andras Beni <andras.beni@streamnative.io>
2023-03-15 09:58:48 +0100

    Depend on io.streamnative:pulsar*:2.11.0.3 (#90)
    * Depend on io.streamnative:pulsar*:2.11.0.3
    
    * Update pulsar-metadatastore-oxia/src/main/java/io/streamnative/pulsarmetadatastoreoxia/OxiaMetadataStore.java
    
    ---------
    
    Co-authored-by: Dave Maughan <davidamaughan@gmail.com>

--- pulsar-metadatastore-oxia/src/main/java/io/streamnative/pulsarmetadatastoreoxia/OxiaMetadataStore.java
+++ pulsar-metadatastore-oxia/src/main/java/io/streamnative/pulsarmetadatastoreoxia/OxiaMetadataStore.java
@@ -54,7 +54,6 @@ public class OxiaMetadataStore extends AbstractMetadataStore {
     OxiaMetadataStore(
             String serviceAddress, MetadataStoreConfig metadataStoreConfig, boolean enableSessionWatcher)
             throws Exception {
-        super(metadataStoreConfig.getMetadataStoreName());
         var linger = metadataStoreConfig.getBatchingMaxDelayMillis();
         if (!metadataStoreConfig.isBatchingEnabled()) {
             linger = 0;
@@ -64,7 +63,6 @@ public class OxiaMetadataStore extends AbstractMetadataStore {
         client =
                 new OxiaClientBuilder(serviceAddress)
                         .clientIdentifier(identity)
-                        /* TODO batchingMaxSizeKb: not supported by oxia _yet_ */
                         .sessionTimeout(Duration.ofMillis(metadataStoreConfig.getSessionTimeoutMillis()))
                         .batchLinger(Duration.ofMillis(linger))
                         .maxRequestsPerBatch(metadataStoreConfig.getBatchingMaxOperations())
@@ -75,7 +73,7 @@ public class OxiaMetadataStore extends AbstractMetadataStore {
     }
 
     private void notificationCallback(Notification notification) {
-        if (notification.key().startsWith("__oxia") || this.isClosed.get()) {
+        if (notification.key().startsWith("__oxia")) {
             return;
         }
         if (notification instanceof Notification.KeyCreated keyCreated) {
@@ -258,12 +256,10 @@ public class OxiaMetadataStore extends AbstractMetadataStore {
 
     @Override
     public void close() throws Exception {
-        if (isClosed.compareAndSet(false, true)) {
-            if (client != null) {
-                client.close();
-            }
-            super.close();
+        if (client != null) {
+            client.close();
         }
+        super.close();
     }
 
     public Optional<MetadataEventSynchronizer> getMetadataEventSynchronizer() {
