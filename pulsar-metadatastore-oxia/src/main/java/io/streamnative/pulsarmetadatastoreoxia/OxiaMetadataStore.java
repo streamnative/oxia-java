d4cf7de0c5f668d5459d1f7ba924fc7e703db245
Matteo Merli <mmerli@apache.org>
2024-05-01 22:21:14 -0700

    Fixed pulsar adapters

--- pulsar-metadatastore-oxia/src/main/java/io/streamnative/pulsarmetadatastoreoxia/OxiaMetadataStore.java
+++ pulsar-metadatastore-oxia/src/main/java/io/streamnative/pulsarmetadatastoreoxia/OxiaMetadataStore.java
@@ -19,6 +19,7 @@ import io.streamnative.oxia.client.OxiaClientBuilder;
 import io.streamnative.oxia.client.api.AsyncOxiaClient;
 import io.streamnative.oxia.client.api.DeleteOption;
 import io.streamnative.oxia.client.api.Notification;
+import io.streamnative.oxia.client.api.OxiaClientBuilder;
 import io.streamnative.oxia.client.api.PutOption;
 import io.streamnative.oxia.client.api.PutResult;
 import io.streamnative.oxia.client.api.Version;
@@ -66,7 +67,7 @@ public class OxiaMetadataStore extends AbstractMetadataStore {
         this.synchronizer = Optional.ofNullable(metadataStoreConfig.getSynchronizer());
         identity = UUID.randomUUID().toString();
         client =
-                new OxiaClientBuilder(serviceAddress)
+                OxiaClientBuilder.create(serviceAddress)
                         .clientIdentifier(identity)
                         .namespace(namespace)
                         .sessionTimeout(Duration.ofMillis(metadataStoreConfig.getSessionTimeoutMillis()))
