d7d19c5f660075db79e03684e3b81d0ca9330a46
Matteo Merli <mmerli@apache.org>
2024-05-03 18:23:46 -0700

    Reimplemented uint32 -> long conversion without allocations (#148)
    * Reimplemented uint32 -> long conversion without allocations
    
    * spotless

--- client/src/test/java/io/streamnative/oxia/client/ProtoUtilTest.java
+++ client/src/test/java/io/streamnative/oxia/client/ProtoUtilTest.java
@@ -30,14 +30,4 @@ class ProtoUtilTest {
         assertThat(ProtoUtil.uint32ToLong(-1)).isEqualTo(4294967295L);
         assertThat(ProtoUtil.uint32ToLong(0)).isEqualTo(0L);
     }
-
-    @Test
-    void longToUnit32() {
-        assertThat(ProtoUtil.longToUint32(1L)).isEqualTo(1);
-        assertThat(ProtoUtil.longToUint32(Integer.MAX_VALUE)).isEqualTo(Integer.MAX_VALUE);
-        assertThat(ProtoUtil.longToUint32(Integer.MAX_VALUE + 1L)).isEqualTo(Integer.MIN_VALUE);
-        assertThat(ProtoUtil.longToUint32(Integer.MAX_VALUE + 2L)).isEqualTo(Integer.MIN_VALUE + 1);
-        assertThat(ProtoUtil.longToUint32(4294967295L)).isEqualTo(-1);
-        assertThat(ProtoUtil.longToUint32(0L)).isEqualTo(0);
-    }
 }
