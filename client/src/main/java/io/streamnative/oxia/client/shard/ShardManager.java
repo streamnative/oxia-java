ce4ddc23a03cdc5c8c47d6a48022dcfd04b48571
Qiang Zhao <mattisonchao@apache.org>
2024-08-02 16:23:58 +0800

    shards: fix shards assign missing updated value (#172)
    * shards: fix shards assign missing updated value
    
    In the old logic, we didn't return the newest shards for reassigned
    collection. which causes client can't connect to expected leader.

--- client/src/main/java/io/streamnative/oxia/client/shard/ShardManager.java
+++ client/src/main/java/io/streamnative/oxia/client/shard/ShardManager.java
@@ -239,6 +239,7 @@ public class ShardManager implements AutoCloseable, StreamObserver<ShardAssignme
                 oldAssignments.values().stream()
                         .filter(s -> newAssignments.containsKey(s.id()))
                         .filter(s -> !newAssignments.get(s.id()).leader().equals(s.leader()))
+                        .map(s -> newAssignments.get(s.id())) // return reassigned leader
                         .collect(toSet());
         return new ShardAssignmentChanges(
                 unmodifiableSet(added), unmodifiableSet(removed), unmodifiableSet(changed));
