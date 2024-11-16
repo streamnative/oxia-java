7ffdddd0809a7415881db2d34db8eec626daad6b
Qiang Zhao <mattisonchao@apache.org>
2024-11-17 02:58:30 +0800

    feat(stream): fail pending request if server close stream without response (#191)
    * feat(stream): fail pending request if server close stream without
    exception
    
    * fix spotless

--- client/src/main/java/io/streamnative/oxia/client/batch/ReadBatch.java
+++ client/src/main/java/io/streamnative/oxia/client/batch/ReadBatch.java
@@ -23,6 +23,7 @@ import io.streamnative.oxia.proto.ReadRequest;
 import io.streamnative.oxia.proto.ReadResponse;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CancellationException;
 import lombok.NonNull;
 
 final class ReadBatch extends BatchBase implements Batch, StreamObserver<ReadResponse> {
@@ -83,6 +84,13 @@ final class ReadBatch extends BatchBase implements Batch, StreamObserver<ReadRes
 
     @Override
     public void onCompleted() {
+        // complete pending request if the server close stream without any response
+        gets.forEach(
+                g -> {
+                    if (!g.callback().isDone()) {
+                        g.fail(new CancellationException());
+                    }
+                });
         factory.getReadRequestLatencyHistogram().recordSuccess(System.nanoTime() - startSendTimeNanos);
     }
 
