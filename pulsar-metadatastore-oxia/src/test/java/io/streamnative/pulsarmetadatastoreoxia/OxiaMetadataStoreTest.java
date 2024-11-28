e93ed82b717a8d8fcf75019fb503500af42d0637
Lari Hotari <lhotari@users.noreply.github.com>
2024-11-28 12:56:23 +0200

    Add Java 21 build support and enable running of pulsar-metadatastore-oxia tests (#194)
    * Update test dependencies, cleanup build and make it buildable with Java 21
    
    * Fix tests
    
    * Run spotless:apply

--- pulsar-metadatastore-oxia/src/test/java/io/streamnative/pulsarmetadatastoreoxia/OxiaMetadataStoreTest.java
+++ pulsar-metadatastore-oxia/src/test/java/io/streamnative/pulsarmetadatastoreoxia/OxiaMetadataStoreTest.java
@@ -67,4 +67,12 @@ public class OxiaMetadataStoreTest extends MetadataStoreTest implements OxiaTest
     @Test(dataProvider = "impl")
     public void testDeleteUnusedDirectories(String provider, Supplier<String> urlSupplier)
             throws Exception {}
+
+    @Ignore
+    @Test(dataProvider = "conditionOfSwitchThread")
+    @Override
+    public void testThreadSwitchOfZkMetadataStore(boolean hasSynchronizer, boolean enabledBatch)
+            throws Exception {
+        super.testThreadSwitchOfZkMetadataStore(hasSynchronizer, enabledBatch);
+    }
 }
--- pulsar-metadatastore-oxia/src/test/java/io/streamnative/pulsarmetadatastoreoxia/bookkeeper/OxiaLedgerUnderreplicationManagerTest.java
+++ pulsar-metadatastore-oxia/src/test/java/io/streamnative/pulsarmetadatastoreoxia/bookkeeper/OxiaLedgerUnderreplicationManagerTest.java
@@ -23,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.metadata.bookkeeper.LedgerUnderreplicationManagerTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Ignore;
 
 /** Test the zookeeper implementation of the ledger replication manager. */
 @Slf4j
@@ -40,4 +41,8 @@ public class OxiaLedgerUnderreplicationManagerTest extends LedgerUnderreplicatio
     public Object[][] implementations() {
         return impl();
     }
+
+    @Ignore
+    @Override
+    public void testZkMetasStoreMarkReplicatedDeleteEmptyParentNodes() throws Exception {}
 }
