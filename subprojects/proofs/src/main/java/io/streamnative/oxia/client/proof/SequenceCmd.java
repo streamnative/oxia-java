574b84ce8aab91849b8875fe803352e5da61602e
Qiang Zhao <mattisonchao@apache.org>
2025-02-24 20:24:12 +0800

    add shards

--- subprojects/proofs/src/main/java/io/streamnative/oxia/client/proof/SequenceCmd.java
+++ subprojects/proofs/src/main/java/io/streamnative/oxia/client/proof/SequenceCmd.java
@@ -19,28 +19,38 @@ import com.google.common.util.concurrent.RateLimiter;
 import io.streamnative.oxia.client.api.AsyncOxiaClient;
 import io.streamnative.oxia.client.api.OxiaClientBuilder;
 import io.streamnative.oxia.client.api.PutOption;
-import lombok.extern.slf4j.Slf4j;
-import picocli.CommandLine;
 
+import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.LockSupport;
+
+import io.streamnative.oxia.client.shard.ShardManager;
+import lombok.extern.slf4j.Slf4j;
+import picocli.CommandLine;
 
 @Slf4j
 @CommandLine.Command(name = "sequence")
 public final class SequenceCmd extends BaseCmd implements Runnable, Exec {
 
     private AsyncOxiaClient client;
+    private ShardManager shardManager;
 
     @Override
     public void run() {
-        this.client = OxiaClientBuilder.create(serviceAddr)
-                .namespace(namespace)
-                .asyncClient()
-                .join();
+        this.client = OxiaClientBuilder.create(serviceAddr).namespace(namespace).asyncClient().join();
+        try {
+            final var field = client.getClass().getDeclaredField("shardManager");
+            field.setAccessible(true);
+            final Object obj = field.get(client);
+            this.shardManager = (ShardManager) obj;
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
         exec();
     }
 
@@ -51,48 +61,83 @@ public final class SequenceCmd extends BaseCmd implements Runnable, Exec {
 
         for (int i = 0; i < keyNum; i++) {
             final String key = UUID.randomUUID().toString();
+            final Semaphore pendingRequest = new Semaphore(10000);
             log.info("starting delta proof test.  key={}", key);
             Thread.ofVirtual()
                     .name("proof-" + key)
-                    .start(() -> {
-                        final AtomicLong payloadCounter = new AtomicLong(0);
-                        final AtomicLong expectDeltaL = new AtomicLong(1);
-                        final AtomicLong expectDeltaM = new AtomicLong(2);
-                        final AtomicLong expectDeltaR = new AtomicLong(3);
-                        log.info("delta proof test is ready. key={}", key);
-                        while (true) {
-                            rateLimiter.acquire();
-                            final ByteBuffer buf = ByteBuffer.allocate(8);
-                            buf.putLong(payloadCounter.getAndIncrement());
-                            final var sendFuture = client.put(key, buf.array(), Set.of(PutOption.SequenceKeysDeltas(List.of(1L, 2L, 3L)), PutOption.PartitionKey(key)));
-                            sendFuture.whenComplete((result, error) -> {
-                                if (error != null) {
-                                    log.warn("[{}] receive put with sequence keys error. {}", key, error.getMessage());
-                                    return;
+                    .start(
+                            () -> {
+                                final AtomicLong payloadCounter = new AtomicLong(0);
+                                final AtomicLong expectDeltaL = new AtomicLong(1);
+                                final AtomicLong expectDeltaM = new AtomicLong(2);
+                                final AtomicLong expectDeltaR = new AtomicLong(3);
+                                log.info("delta proof test is ready. key={}", key);
+                                while (true) {
+                                    rateLimiter.acquire();
+                                    try {
+                                        pendingRequest.acquire();
+                                    } catch (InterruptedException ex) {
+                                        log.error("delta proof test interrupted. {}", ex.getMessage());
+                                        throw new RuntimeException(ex);
+                                    }
+                                    final ByteBuffer buf = ByteBuffer.allocate(8);
+                                    buf.putLong(payloadCounter.getAndIncrement());
+                                    final var sendFuture =
+                                            client.put(
+                                                    key,
+                                                    buf.array(),
+                                                    Set.of(
+                                                            PutOption.SequenceKeysDeltas(List.of(1L, 2L, 3L)),
+                                                            PutOption.PartitionKey(key)));
+                                    sendFuture.whenComplete(
+                                            (result, error) -> {
+                                                pendingRequest.release();
+                                                if (error != null) {
+                                                    log.warn(
+                                                            "[{}][{}] receive put with sequence keys error. {}",
+                                                            key,
+                                                            shardManager.getShardForKey(key),
+                                                            error.getMessage());
+                                                    return;
+                                                }
+                                                final String deltaKey = result.key();
+                                                final String[] deltas = deltaKey.split("-");
+                                                final long deltaL = Long.parseLong(deltas[1]);
+                                                final long deltaM = Long.parseLong(deltas[2]);
+                                                final long deltaR = Long.parseLong(deltas[3]);
+                                                if (deltaL != expectDeltaL.longValue()) {
+                                                    log.warn(
+                                                            "[{}][{}] detected unexpected delta(L)  expect: {} , actual: {}",
+                                                            key,
+                                                            shardManager.getShardForKey(key),
+                                                            expectDeltaL.get(),
+                                                            deltaL);
+                                                    expectDeltaL.set(deltaL);
+                                                }
+                                                if (deltaM != expectDeltaM.longValue()) {
+                                                    log.warn(
+                                                            "[{}][{}] detected unexpected delta(M):  expect: {} , actual: {}",
+                                                            key,
+                                                            shardManager.getShardForKey(key),
+                                                            expectDeltaM.get(),
+                                                            deltaM);
+                                                    expectDeltaM.set(deltaM);
+                                                }
+                                                if (deltaR != expectDeltaR.longValue()) {
+                                                    log.warn(
+                                                            "[{}][{}] detected unexpected delta(R):  expect: {} , actual: {}",
+                                                            key,
+                                                            shardManager.getShardForKey(key),
+                                                            expectDeltaR.get(),
+                                                            deltaR);
+                                                    expectDeltaR.set(deltaR);
+                                                }
+                                                expectDeltaL.addAndGet(1);
+                                                expectDeltaM.addAndGet(2);
+                                                expectDeltaM.addAndGet(3);
+                                            });
                                 }
-                                final String deltaKey = result.key();
-                                final String[] deltas = deltaKey.split("-");
-                                final long deltaL = Long.parseLong(deltas[1]);
-                                final long deltaM = Long.parseLong(deltas[2]);
-                                final long deltaR = Long.parseLong(deltas[3]);
-                                if (deltaL != expectDeltaL.longValue()) {
-                                    log.warn("[{}] detected unexpected delta(L)  expect: {} , actual: {}", key, expectDeltaL.get(), deltaL);
-                                    expectDeltaL.set(deltaL);
-                                }
-                                if (deltaM != expectDeltaM.longValue()) {
-                                    log.warn("[{}] detected unexpected delta(M):  expect: {} , actual: {}", key, expectDeltaM.get(), deltaM);
-                                    expectDeltaM.set(deltaM);
-                                }
-                                if (deltaR != expectDeltaR.longValue()) {
-                                    log.warn("[{}] detected unexpected delta(R):  expect: {} , actual: {}", key, expectDeltaR.get(), deltaR);
-                                    expectDeltaR.set(deltaR);
-                                }
-                                expectDeltaL.addAndGet(1);
-                                expectDeltaM.addAndGet(2);
-                                expectDeltaM.addAndGet(3);
                             });
-                        }
-                    });
         }
         LockSupport.park();
     }
